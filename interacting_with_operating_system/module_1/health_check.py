09c0b9260080a2c6643d1f27d6e2942f1df3eb29
UcheWisdom <uchebuakuwisdom@gmail.com>
2026-06-16 02:29:50 +0100

    health check

--- interacting_with_operating_system/module_1/health_check.py
+++ interacting_with_operating_system/module_1/health_check.py
@@ -1,17 +1,3 @@
-# import shutil
-# import psutil
-
-# du = shutil.disk_usage("/")
-# print(du)
-
-# # Print the percentage of free space
-# print(f"Percentage of free space: {du.free/du.total*100:.2f}%")
-# # print cpu usage
-# print(psutil.cpu_percent(0.1)) 
-
-
-#Health checking script
-#!/usr/bin/env python3
 import shutil
 import psutil
 
