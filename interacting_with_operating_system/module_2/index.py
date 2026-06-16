b5cea70c35138007ebd7e0ee7042c8b0d01ffed3
UcheWisdom <uchebuakuwisdom@gmail.com>
2026-06-16 23:15:53 +0100

    website repo

--- interacting_with_operating_system/module_2/index.py
+++ interacting_with_operating_system/module_2/index.py
@@ -24,17 +24,50 @@ import datetime
 
 # os.path.exists("./spider_renamed.txt")
 
-#This code will provide the file size
-print(os.path.getsize("spider.txt"))
+# #This code will provide the file size
+# print(os.path.getsize("spider.txt"))
 
-#This code will provide a unix timestamp for the file unix timestamps
-print(os.path.getmtime("spider.txt"))
+# #This code will provide a unix timestamp for the file unix timestamps
+# print(os.path.getmtime("spider.txt"))
 
-#This code will provide the date and time for the file in an 
-#easy-to-understand format
-timestamp = os.path.getmtime("spider.txt")
-print(datetime.datetime.fromtimestamp(timestamp))
+# #This code will provide the date and time for the file in an 
+# #easy-to-understand format
+# timestamp = os.path.getmtime("spider.txt")
+# print(datetime.datetime.fromtimestamp(timestamp))
 
 
-#This code takes the file name and turns it into an absolute path
-print(os.path.abspath("spider.txt"))
+# #This code takes the file name and turns it into an absolute path
+# print(os.path.abspath("spider.txt"))
+
+#This code snippet returns the current working directory.
+print(os.getcwd())
+
+
+#The os.mkdir("new_dir") function creates a new directory called new_dir
+os.mkdir("new_dir")
+
+#This code snippet changes the current working directory to new_dir. 
+#The second line prints the current working directory.
+os.chdir("new_dir")
+os.getcwd()
+
+
+#This code snippet creates a new directory called newer_dir. 
+#The second line deletes the newer_dir directory.
+os.mkdir("newer_dir")
+os.rmdir("newer_dir")
+
+#This code snippet returns a list of all the files and 
+#sub-directories in the website directory.
+import os
+os.listdir("website")
+
+dir = "website"
+for name in os.listdir(dir):
+    fullname = os.path.join(dir, name)
+    if os.path.isdir(fullname):
+        print("{} is a directory".format(fullname))
+    else:
+        print("{} is a file".format(fullname))
+
+        
