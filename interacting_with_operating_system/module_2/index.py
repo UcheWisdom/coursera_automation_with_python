5b594e0d005c5eae0e953c2ac5789be0dfd947cb
UcheWisdom <uchebuakuwisdom@gmail.com>
2026-06-16 18:23:57 +0100

    remove and rename using os

--- interacting_with_operating_system/module_2/index.py
+++ interacting_with_operating_system/module_2/index.py
@@ -1,16 +1,25 @@
-file =open("./spider.txt")
-print(file.readline())
-file.close()
+# file =open("./spider.txt")
+# print(file.readline())
+# file.close()
 
-with open("./spider.txt") as file:
-    for line in file:
-        print(line.strip().upper())
+# with open("./spider.txt") as file:
+#     for line in file:
+#         print(line.strip().upper())
 
-file = open("./spider.txt")
-lines = file.readlines()
-file.close()
-lines.sort()
-print(lines)
+# file = open("./spider.txt")
+# lines = file.readlines()
+# file.close()
+# lines.sort()
+# print(lines)
 
-with open("./novel.txt", "w") as file:
-    file.write(("It was a dark and stormy night"))
+# with open("./novel.txt", "w") as file:
+#     file.write(("It was a dark and stormy night"))
+
+import os
+
+# os.remove("./novel.txt")
+# os.remove("./novel.txt")
+
+os.rename("./spider_renamed.txt", "./spider.txt")
+
+os.path.exists("./spider_renamed.txt")
