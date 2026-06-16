9e268d55321051edb9724460530b5f2ab080d8d3
UcheWisdom <uchebuakuwisdom@gmail.com>
2026-06-16 18:10:25 +0100

    add index

--- interacting_with_operating_system/module_2/index.py
+++ interacting_with_operating_system/module_2/index.py
@@ -1,7 +1,16 @@
 file =open("./spider.txt")
 print(file.readline())
-
 file.close()
 
-with open("spider.txt") as file:
-    print(file.readline())
+with open("./spider.txt") as file:
+    for line in file:
+        print(line.strip().upper())
+
+file = open("./spider.txt")
+lines = file.readlines()
+file.close()
+lines.sort()
+print(lines)
+
+with open("./novel.txt", "w") as file:
+    file.write(("It was a dark and stormy night"))
