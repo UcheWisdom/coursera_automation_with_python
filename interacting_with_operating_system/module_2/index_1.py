eeaee792baf36e24c37507bea28a5c06468202af
UcheWisdom <uchebuakuwisdom@gmail.com>
2026-06-18 12:13:22 +0100

    reading a csv file

---
+++ interacting_with_operating_system/module_2/index_1.py
@@ -0,0 +1,11 @@
+import csv
+
+f = open("./csv_file.txt")
+csv_f = csv.reader(f)
+for row in csv_f:
+    name, phone, role = row
+    print("Name: {}, Phone: {}, Role: {}".format(name, phone, role))
+f.close()
+
+
+
