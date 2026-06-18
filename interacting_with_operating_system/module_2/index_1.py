b3685d955aedb32ae3584f7a7eb11db316dc32fe
UcheWisdom <uchebuakuwisdom@gmail.com>
2026-06-18 12:19:57 +0100

    generate csv file

--- interacting_with_operating_system/module_2/index_1.py
+++ interacting_with_operating_system/module_2/index_1.py
@@ -9,3 +9,7 @@ f.close()
 
 
 
+hosts = [["workstation.locl", "192.168.25.46"],["webserver.cloud", "10.2.5.6"]]
+with open('hosts.csv', "w") as host_csv:
+    writer = csv.writer(host_csv)
+    writer.writerows(hosts) 
